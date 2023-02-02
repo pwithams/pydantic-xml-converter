35ad65e769fec5191b32e776d685279fcefb5d73
Patrick Withams <pwithams@gmail.com>
2023-02-02 00:31:02 -0700

    v0.0.8

--- pydantic_xml/__init__.py
+++ pydantic_xml/__init__.py
@@ -6,7 +6,7 @@ import xmltodict
 from pydantic import BaseModel, ValidationError
 from pydantic.fields import ModelField
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 
 class XmlAttribute(BaseModel):
